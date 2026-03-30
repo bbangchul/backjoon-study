7f9ddb5a6405bf4c173ac29aa160fe202808cfc7
bbangchul <bangsc02@gmail.com>
2026-03-30 19:49:24 +0900

    4949.cpp

---
+++ 05_bbakking/0x08/4949.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    while (1)
+    {
+        string a;
+        getline(cin, a);
+        if (a == ".")
+            break;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    solve();
+}
