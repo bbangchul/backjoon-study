aeca6592a6d825a2fe77f0e7c637d80d9c9565fc
bbangchul <bangsc02@gmail.com>
2026-04-13 16:43:14 +0900

    1271.cpp

---
+++ deu/ascode/1271.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+
+void solve()
+{
+    int n;
+    ll m;
+    cin >> n >> m;
+
+    vector<ll> s(n), v(n);
+    ll lo = (ll)4e18, hi = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> s[i];
+        lo = min(lo, s[i]);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+        hi = max(hi, s[i] + m * v[i]);
+    }
+
+    auto ok = [&](ll x)
+    {
+        ll need = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] >= x)
+                continue;
+            need += (x - s[i] + v[i] - 1) / v[i];
+            if (need > m)
+                return false;
+        }
+        return true;
+    };
+
+    ll ans = lo;
+    while (lo <= hi)
+    {
+        ll mid = lo + (hi - lo) / 2;
+        if (ok(mid))
+        {
+            ans = mid;
+            lo = mid + 1;
+        }
+        else
+        {
+            hi = mid - 1;
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+}
